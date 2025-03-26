01a4a30fbf74b32a221e9c50b6d4bc7e63c4d750
dungdaidungcach <tungmj1605@gmail.com>
2025-03-26 14:58:04 +0700

    git inti

---
+++ lib/features/auth/view/pages/signup_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: Center(
+        child: Text('Signup Form Goes Here'),
+      ),
+    );
+  }
+}
