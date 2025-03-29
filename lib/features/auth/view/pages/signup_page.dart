b0741539e2d02851ef46a257dbf96150c2ee66a0
dungdaidungcach <tungmj1605@gmail.com>
2025-03-30 04:00:24 +0700

    Updated the look and functionality of the login page, converting it from a login page to a registration page with data fields and a registration button. Changed the app theme to dark theme.

--- lib/features/auth/view/pages/signup_page.dart
+++ lib/features/auth/view/pages/signup_page.dart
@@ -1,16 +1,87 @@
 import 'package:flutter/material.dart';
 
-class SignupPage extends StatelessWidget {
+import '../../../../core/theme/app_pallete.dart';
+import '../widgets/auth_gradient_button.dart';
+import '../widgets/custom_field.dart';
+
+class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
-      body: Center(
-        child: Text('Signup Form Goes Here'),
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30),
+              CustomField(
+                hintText: 'Enter your name',
+                controller: nameController,
+              ),
+              SizedBox(height: 15),
+              CustomField(
+                hintText: 'Enter your email',
+                controller: emailController,
+              ),
+              SizedBox(height: 15),
+              CustomField(
+                hintText: 'Enter your password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              SizedBox(height: 20),
+              AuthGradientButton(
+                buttonText: 'Sign Up',
+                onTap: () {},
+              ),
+              SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: "Already have a account ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: "Sign In ",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                            color: Pallete.gradient2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone_client/core/theme/theme.dart';
 
-import 'features/auth/view/pages/signup_page.dart';
+import 'features/auth/view/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,11 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SignupPage(),
+      theme: AppTheme.darkThemeMode,
+      home: const LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
